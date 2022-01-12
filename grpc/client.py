dce16f684147f235bb66a69666ded3cb83c61ad4
breno-cn <breno.cn@hotmail.com>
2022-01-11 23:16:16 -0300

    removendo código comentado

--- grpc/client.py
+++ grpc/client.py
@@ -2,22 +2,6 @@ from hashtable_pb2_grpc import *
 from hashtable_pb2 import *
 import grpc
 
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
-# with grpc.insecure_channel('localhost:50051') as channel:
-#     stub = hashtable_pb2_grpc.HashtableStub(channel)
-#     response = stub.read(hashtable_pb2.ReadRequest(key='nome'))
-#     print("Greeter client received: " + str(response))
-
-#     response = stub.update(hashtable_pb2.UpdateRequest(key='nome', value='David'))
-#     print(str(response))
-
-#     response = stub.delete(hashtable_pb2.DeleteRequest(key='nome'))
-#     print(str(response))
-
-#     response = stub.read(hashtable_pb2.ReadRequest(key='nome'))
-#     print(str(response))
 
 with grpc.insecure_channel('localhost:50051') as channel:
     stub = HashtableStub(channel)
